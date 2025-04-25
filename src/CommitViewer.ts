be7f0044281a4bf5dba6e81caaa919d14640dcd7
autecht <autecht@ucsd.edu>
2025-04-24 18:37:07 -0700

    refactor code: use async exec() calls and abstract out reused logic

--- src/CommitViewer.ts
+++ src/CommitViewer.ts
@@ -2,215 +2,184 @@ import * as vscode from "vscode";
 import { exec } from "child_process";
 import * as util from "util";
 
-
-
-
 interface CommitInfo {
   hash: string;
   message: string;
 }
 
-
-
-
 class CommandExecutor {
   context: vscode.ExtensionContext;
   workspaceRoot: vscode.WorkspaceFolder | undefined;
   commitViewer: CommitViewer;
-  constructor(context:vscode.ExtensionContext, commitViewer: CommitViewer) {
-      this.commitViewer = commitViewer;
-      this.context = context;
-      const workspaceFolders = vscode.workspace.workspaceFolders;
-      if (!workspaceFolders) {
-        return;
-      }
-      const workspaceRoot = workspaceFolders[0];
-      this.workspaceRoot = workspaceRoot;
-
+  constructor(context: vscode.ExtensionContext, commitViewer: CommitViewer) {
+    this.commitViewer = commitViewer;
+    this.context = context;
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+    if (!workspaceFolders) {
+      return;
     }
+    const workspaceRoot = workspaceFolders[0];
+    this.workspaceRoot = workspaceRoot;
+  }
 
-  async executeCommand(command: string):Promise<string> {
+  async executeCommand(command: string): Promise<string> {
     const promisifiedExec = util.promisify(exec);
     const output = await promisifiedExec(command, {
-      cwd: this.workspaceRoot?.uri.fsPath,});
+      cwd: this.workspaceRoot?.uri.fsPath,
+    });
     return output.stdout;
   }
 
-  executeDiffCommand(message: CommitInfo) {
-    exec(
-              "git diff-tree --no-commit-id --name-only -r " + message.hash,
-              { cwd: this.workspaceRoot?.uri.fsPath },
-              (error, stdout, stderr) => {
-                if (error) {
-                  return;
-                }
-                console.log("stdout", stdout);
-                const filesChanged = stdout
-                  .split("\n")
-                  .filter((file) => file.trim() !== "");
-                
-                if (!this.workspaceRoot) {
-                  return;
-                }
+  async executeDiffCommand(message: CommitInfo) {
+    console.log("Executing diff command for commit:", message.hash);
+    const output = await this.executeCommand(
+      `git diff-tree --no-commit-id --name-only -r ${message.hash}`
+    );
+    const filesChanged = output
+      .split("\n")
+      .filter((file) => file.trim() !== "");
+    if(filesChanged.length === 0) {
+      vscode.window.showInformationMessage("No files changed in this commit.");
+      return;
+    }
+    if (!this.workspaceRoot) {
+      console.log("No workspace root found.");
+      return;
+    }
 
-                for (const file of filesChanged) {
-                  const absolute = vscode.Uri.joinPath(this.workspaceRoot.uri, file);
-                  // For files in the repo root
-                  const params = {
-                    path: absolute.fsPath,
-                    ref: message.hash,
-                  };
-                  const path = absolute.path;
+    for (const file of filesChanged) {
+      const absolute = vscode.Uri.joinPath(this.workspaceRoot.uri, file);
+      // For files in the repo root
+      const params = {
+        path: absolute.fsPath,
+        ref: message.hash,
+      };
+      const path = absolute.path;
 
-                  const gitUri = absolute.with({
-                    scheme: "git",
-                    path,
-                    query: JSON.stringify(params),
-                  });
+      const gitUri = absolute.with({
+        scheme: "git",
+        path,
+        query: JSON.stringify(params),
+      });
 
-                  // Open diff view
-                  vscode.commands.executeCommand(
-                    "vscode.diff",
-                    gitUri,
-                    absolute,
-                    `Diff ${file}: ${message.hash} -> present`
-                  );
-                }
-              }
-            );
-   }
+      console.log("About to open diff view");
+      // Open diff view
+      vscode.commands.executeCommand(
+        "vscode.diff",
+        gitUri,
+        absolute,
+        `Diff ${file}: ${message.hash} -> present`
+      );
+    }
+  }
 
-   executeLogCommand(panel: vscode.WebviewPanel, hash?: string) {
+  async executeLogCommand(panel: vscode.WebviewPanel, hash?: string) {
+    const command =
+      hash === undefined
+        ? 'git log --pretty="%h "%s'
+        : `git log -n 1 --pretty="%h "%s ${hash}`;
+    const stylesheetPath = vscode.Uri.joinPath(
+      this.context.extensionUri,
+      "media",
+      "commit-view.css"
+    );
+    const stylesheetUri = panel.webview.asWebviewUri(stylesheetPath);
 
-        const command = hash===undefined?'git log --pretty="%h "%s':`git log -n 1 --pretty="%h "%s ${hash}`;
-        const stylesheetPath = vscode.Uri.joinPath(
-          this.context.extensionUri,
-          "media",
-          "commit-view.css"
-        );
-        const stylesheetUri = panel.webview.asWebviewUri(stylesheetPath);
+    const scriptPath = vscode.Uri.joinPath(
+      this.context.extensionUri,
+      "media",
+      "commit-view.js"
+    );
+    const scriptUri = panel.webview.asWebviewUri(scriptPath);
 
-        const scriptPath = vscode.Uri.joinPath(
-          this.context.extensionUri,
-          "media",
-          "commit-view.js"
-        );
-        const scriptUri = panel.webview.asWebviewUri(scriptPath);
+    const output = await this.executeCommand(command);
+    const commits = this.getCommitInfo(output);
 
-        
-        
-        const workspaceRootString = this.workspaceRoot?.uri.fsPath;
-        if (!workspaceRootString) {return;}
-        exec(command, { cwd: workspaceRootString }, (error, stdout, stderr) => {
-          if (error) {
-            return;
-          }
-          const commits = this.getCommitInfo(stdout);
-          panel.webview.html = this.commitViewer.getViewContent(
-            stylesheetUri,
-            scriptUri,
-            commits
-          );
-        });
-   
-   }
+    panel.webview.html = this.commitViewer.getViewContent(
+      stylesheetUri,
+      scriptUri,
+      commits
+    );
+  }
 
   /**
    * Parses the output of the git log command to extract commit information.
-    * @param stdout The output of the git log command.
-    * @returns An array of CommitInfo objects containing the commit hash and message.
-    */
-   getCommitInfo(stdout: string): CommitInfo[] {
-      const commitLines = stdout
-        .split("\n")
-        .filter((line) => line.trim() !== "");
-      const commits: CommitInfo[] = commitLines.map((line) => {
-        const [hash, ...messageParts] = line.split(" ");
-        const message = messageParts.join(" ");
-        return { hash, message };
-      });
-      return commits;
+   * @param stdout The output of the git log command.
+   * @returns An array of CommitInfo objects containing the commit hash and message.
+   */
+  getCommitInfo(stdout: string): CommitInfo[] {
+    const commitLines = stdout.split("\n").filter((line) => line.trim() !== "");
+    const commits: CommitInfo[] = commitLines.map((line) => {
+      const [hash, ...messageParts] = line.split(" ");
+      const message = messageParts.join(" ");
+      return { hash, message };
+    });
+    return commits;
   }
 }
 
-
-
 class CommitViewer {
-
   showCommitsCommand: vscode.Disposable;
   showCommitCommand: vscode.Disposable;
   commandExecutor: CommandExecutor;
 
   constructor(context: vscode.ExtensionContext) {
-
     this.commandExecutor = new CommandExecutor(context, this);
+    this.handleMessage = this.handleMessage.bind(this); // Bind the method to the class instance
+    
     this.showCommitsCommand = vscode.commands.registerCommand(
       "contextawareversioncontrol.showCommits",
       () => {
-        const panel = vscode.window.createWebviewPanel(
+        const panel = this.createWebviewPanel(
+          context,
           "showCommits",
-          "Relevant Commits",
-          vscode.ViewColumn.One,
-          {
-            enableScripts: true,
-            localResourceRoots: [
-              vscode.Uri.joinPath(context.extensionUri, "media"),
-            ],
-          }
+          "Relevant Commits"
         );
-
-        panel.webview.onDidReceiveMessage((message) => {
-          if (message.command === "openDiffFile") {
-            console.log("Message received");
-
-            const workspaceFolders = vscode.workspace.workspaceFolders;
-            if (!workspaceFolders) {
-              return;
-            }
-            const workspaceRoot = workspaceFolders[0].uri.fsPath;
-            this.commandExecutor.executeDiffCommand(message);
-            
-          }
-        });
+        panel.webview.onDidReceiveMessage(this.handleMessage);
         this.commandExecutor.executeLogCommand(panel);
       }
     );
-
+    
     this.showCommitCommand = vscode.commands.registerCommand(
       "contextawareversioncontrol.showCommit",
       (hash) => {
-        console.log("showCommit command called");
-        const panel = vscode.window.createWebviewPanel(
+        const panel = this.createWebviewPanel(
+          context,
           "showCommit",
-          "Selected Commit",
-          vscode.ViewColumn.One,
-          {
-            enableScripts: true,
-            localResourceRoots: [
-              vscode.Uri.joinPath(context.extensionUri, "media"),
-            ],
-          }
+          "Selected Commit"
         );
-
-        panel.webview.onDidReceiveMessage((message) => {
-          if (message.command === "openDiffFile") {
-            console.log("Message received");
-
-            const workspaceFolders = vscode.workspace.workspaceFolders;
-            if (!workspaceFolders) {
-              return;
-            }
-            const workspaceRoot = workspaceFolders[0].uri.fsPath;
-            this.commandExecutor.executeDiffCommand(message);
-            
-          }
-        });
+        panel.webview.onDidReceiveMessage(this.handleMessage);
         this.commandExecutor.executeLogCommand(panel, hash);
       }
     );
-
   }
-  
+
+
+  handleMessage(message: any) {
+    if (message.command === "openDiffFile") {
+      vscode.window.showInformationMessage("Open Diff file command Message received in webview");
+      console.log("Message received");
+      this.commandExecutor.executeDiffCommand(message);
+    }
+  }
+  createWebviewPanel(
+    context: vscode.ExtensionContext,
+    identifier: string,
+    title: string
+  ) {
+    const panel = vscode.window.createWebviewPanel(
+      identifier,
+      title,
+      vscode.ViewColumn.One,
+      {
+        enableScripts: true,
+        localResourceRoots: [
+          vscode.Uri.joinPath(context.extensionUri, "media"),
+        ],
+      }
+    );
+    return panel;
+  }
 
   /**
        * 
@@ -251,7 +220,5 @@ class CommitViewer {
   }
 }
 
-
 export { CommitViewer };
-  export { CommitInfo, CommandExecutor };
-
+export { CommitInfo, CommandExecutor };
