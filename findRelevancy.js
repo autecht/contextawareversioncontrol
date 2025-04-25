fae3ab78bf1768f22622d9cc26f01f4bd0944674
Obychenkov <obychenkov@ucsd.edu>
2025-04-24 23:13:14 -0700

    Reformatted findRelevancy to be a function that checks across all files for a given commit, author metric temporarily disabled

--- findRelevancy.js
+++ findRelevancy.js
@@ -1,91 +1,113 @@
-let authorMult = 0.5;
-let timeMult = 0.8;
-let locationMult = 0.9;
+/**
+ * Find relevancy between the current state of the commit and one other commit
+ * @param {String} diffFile .diff file for current commit
+ * @param {String} userFile current file user is editing
+ * @param {Date} commitTime time when commit was made, use "git log -1 --format=%ci [commit hash]""
+ * @param {String} authorName author of commit
+ * @param {Int} startLine first line in user's current viewing window
+ * @param {Int} endLine last line in user's current viewing window
+ * @param {Float[]} mults array representing all the current relevancy settings 
+ * in the order of author, time, location. (and any future metrics)
+ * 
+ * @returns float representing relevancy of current file with 1 being very relevant and 
+ * 0 being not relevant at all
+ */
 
-//Handle time since independent of actual changes
-let commitTime = new Date('2025-04-16 12:19:08 -0700'); //get this using git log -1 --format=%ci [commit hash]
-let latestTime = new Date('2025-04-13 15:40:18 -0700'); //just using the very first commit made
-let curTime = new Date();
-let timePassedScaled = (1 - (curTime - commitTime) / (curTime - latestTime)) * timeMult;
+function findRelevancy(diffFile, userFile, commitTime, authorName, startLine, endLine, mults) {
+    let authorMult = mults[0];
+    let timeMult = mults[1];
+    let locationMult = mults[2];
 
-let relevancy = [0, timePassedScaled, 0]; //[author, time, location]
+    //Handle time since independent of actual changes
+    let latestTime = new Date('2025-04-13 15:40:18 -0700'); //just using the very first commit made
+    let curTime = new Date();
+    let timePassedScaled = (1 - (curTime - commitTime) / (curTime - latestTime)) * timeMult;
 
-//general setup
-const fs = require('fs');
-const readline = require('readline');
-const parse = require('git-diff-parser');
+    let relevancy = [0, timePassedScaled, 0]; //[author, time, location]
 
-const diff = fs.readFileSync('test.diff', 'utf8');
+    //general setup
+    const fs = require('fs');
+    const parse = require('git-diff-parser');
 
-const parsed = parse(diff);
+    const diff = fs.readFileSync(diffFile, 'utf8');
 
-let blameAuthors = getBlameAuthors('blame.txt');
+    const parsed = parse(diff);
 
-let startLine = 30; //using placeholders as current viewing window for now
-let endLine = 50;
+    //let startLine = 30; //using placeholders as current viewing window for now
+    //let endLine = 50;
 
-const lines = parsed['commits'][0]['files'][0]['lines'];
-const filteredLines = lines.filter(line => line.type !== 'normal');
+    const standard = parsed['commits'][0]['files'];
+    let author = 0;
+    let location = 0;
+    let numAuthorLines = 0;
+    let numLocationLines = 0;
 
-let authorName = 'autecht';
-let author = 0;
-let location = 0;
-let totalFileLines = lines.length;
-let numAuthorLines = lines.filter(line => line.type === 'deleted').length;
-let numLocationLines = filteredLines.length;
+    for (let curFile = 0; curFile < standard.length; ++curFile) {
+        const fileName = standard[curFile]['name'];
+        const lines = standard[curFile]['lines'];
+        const filteredLines = lines.filter(line => line.type !== 'normal');
 
-//console.log(numAuthorLines);
-//console.log(numLocationLines);
-//console.log(filteredLines);
+        //let blameAuthors = getBlameAuthors(fileName); 
+        //this wouldn't work directly since I can't call git in a .js file, but 
+        //should work fine when integrated into the main files
+        
+        let totalFileLines = lines.length;
+        numAuthorLines += lines.filter(line => line.type === 'deleted').length;
+        numLocationLines += filteredLines.length;
 
-for (let i = 0; i < filteredLines.length; ++i) {
-    let curChange = filteredLines[i];
-    let curLine = curChange['ln1'];
-    
-    //author check
-    if (curChange['type'] === 'deleted') {
-        author += blameAuthors[curLine] === authorName ? 1 : 0;
+        for (let i = 0; i < filteredLines.length; ++i) {
+            let curChange = filteredLines[i];
+            let curLine = curChange['ln1'];
+            
+            //author check
+            if (curChange['type'] === 'deleted') {
+                //author += blameAuthors[curLine] === authorName ? 1 : 0;
+            }
+
+            //location calculation
+            if (fileName === userFile) {
+                if (curLine >= startLine && curLine <= endLine) {
+                    location += 1;
+                } else if (curLine < startLine) {
+                    location += 1 - ((startLine - curLine) / startLine);
+                } else {
+                    location += 1 - ((curLine - endLine) / (totalFileLines));
+                }
+            }
+        }
     }
 
-    //location calculation
-    if (curLine >= startLine && curLine <= endLine) {
-        location += 1;
-    } else if (curLine < startLine) {
-        location += 1 - ((startLine - curLine) / startLine);
-    } else {
-        location += 1 - ((curLine - endLine) / (totalFileLines));
-    }
+    relevancy[0] = author / numAuthorLines * authorMult;
+    relevancy[2] = location / numLocationLines * locationMult;
+
+    console.log(relevancy);
+
+    //standard L2 distance normalized between 1 (close -> very relevant) and 0 (far -> irrelevant)
+    const dist = Math.sqrt(relevancy[0]**2 + relevancy[1]**2 + relevancy[2]**2) / Math.sqrt(3);
+
+    return dist;
 }
 
-relevancy[0] = author / numAuthorLines * authorMult;
-relevancy[2] = location / numLocationLines * locationMult;
-
-//outputs
-console.log(relevancy);
-
-//standard L2 distance normalized between 0 (close -> very relevant) and 1 (far -> irrelevant)
-const dist = Math.sqrt(
-    [1, 1, 1].reduce((sum, value, index) => sum + Math.pow(value - relevancy[index], 2), 0)
-) / Math.sqrt(3);
-
-console.log(dist);
-
-//Handle git blame to be used later for author relevancy
-//this should also be realistically done once before starting relevancy calculation
-//not for each individual comparison
+/**
+ * 
+ * @param {String} filePath path to the blame file
+ * @returns array of strings of author names in the order of lines
+ */
 //git blame [filename] > blame.txt
 function getBlameAuthors(filePath) {
     const fileContent = fs.readFileSync(filePath, 'utf8');
     const lines = fileContent.split('\n');
-  
+
     let authors = [null];
-  
+
     lines.forEach((line) => {
         const startIndex = line.indexOf('(');
-        const substringAfterParenthesis = line.slice(startIndex + 1).trim();  // trim to remove leading spaces
+        const substringAfterParenthesis = line.slice(startIndex + 1).trim();
         const author = substringAfterParenthesis.split(' ')[0].replace(/\x00/g, '');
         authors.push(author);
     });
 
     return authors;
 }
+
+console.log(findRelevancy("test.diff", "src/CommitViewer.ts", new Date('2025-04-24 20:40:18 -0700'), 'autecht', 30, 50, [0.5, 0.3, 0.8]));
